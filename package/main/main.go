eecd230cc81c7d33f7b46f0bd4c22a2075ab87e0
Zi Wang <ziwang@uber.com>
2017-04-04 00:45:59 -0700

    package

---
+++ package/main/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"github.com/golang-complete-references/package/stringutil"
+)
+
+func main() {
+	fmt.Println(stringutil.Reverse("!oG, olleH!"))
+	fmt.Println(stringutil.MyName)
+}
+
+
